1946ee51f282f8b1f642e1ce0e4c37c4cb04306b
TsubasaHiga <info@higatsubasa.com>
2021-01-28 21:56:07 +0900

    Quicintと同期｜js

--- src/assets/js/page/top.js
+++ src/assets/js/page/top.js
@@ -1,20 +1,5 @@
 'use strict'
 
-import 'swiper/swiper-bundle.min.css'
-import Swiper from 'swiper/bundle'
-
 export default () => {
   console.log('top')
-
-  const mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
-    }
-  })
 }
